d44ccc4f0572f350f952f292c1e0ce1b3cb454e4
Rithiniha kamalanathan <164982399+Rithiniha@users.noreply.github.com>
2025-07-08 11:34:01 +0530

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,23 @@
+function A(callback){
+    setTimeout(() => { console.log("1");
+        callback();
+  }, 3000);}
+function B(callback){
+    setTimeout(() => {console.log("2");
+        callback();
+  }, 2000);}
+function C(){
+    setTimeout(() => { console.log("3");
+  }, 1000);}
+function final(A1,B1,C1) {
+  if ( typeof A1==="function" && typeof B1==="function" && typeof C1=== "function"){
+   A1(() => {
+        B1(() => {
+            C1();
+    });
+})}
+else {
+    console.log("The passed parameters are not functions")
+}
+}
+final(5,B,C)
